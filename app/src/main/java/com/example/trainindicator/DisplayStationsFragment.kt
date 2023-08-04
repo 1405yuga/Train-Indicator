0e9c64fb8e96849ced72601a4085354c36dfca4e
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-08-04 17:09:06 +0530

    applying binding

--- app/src/main/java/com/example/trainindicator/DisplayStationsFragment.kt
+++ app/src/main/java/com/example/trainindicator/DisplayStationsFragment.kt
@@ -5,15 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.trainindicator.databinding.FragmentDisplayStationsBinding
 
 class DisplayStationsFragment : Fragment() {
 
+    private lateinit var binding: FragmentDisplayStationsBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_display_stations, container, false)
+        binding = FragmentDisplayStationsBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
 }
