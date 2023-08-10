9777418a2cd265efb79ca7104e9dcd705bb0d118
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-08-10 13:49:47 +0530

    created firebase function to get route

--- app/src/main/java/com/example/trainindicator/constants/ProjectConstants.kt
+++ app/src/main/java/com/example/trainindicator/constants/ProjectConstants.kt
@@ -5,5 +5,6 @@ object ProjectConstants {
     const val FAST = "FAST"
     const val WESTERN_RAILWAY = "WR"
     const val CENTRAL_RAILWAY = "CR"
+    const val ROUTES = "ROUTES"
     const val EARTH_RADIUS_IN_KM = 6371.0
 }
--- app/src/main/java/com/example/trainindicator/firebase/FirestoreFunctions.kt
+++ app/src/main/java/com/example/trainindicator/firebase/FirestoreFunctions.kt
@@ -3,6 +3,7 @@ package com.example.trainindicator.firebase
 import android.content.Context
 import android.util.Log
 import android.widget.Toast
+import com.example.trainindicator.constants.ProjectConstants
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 
@@ -22,6 +23,24 @@ object FirestoreFunctions {
             }
             .addOnFailureListener {
                 Toast.makeText(context, "Failed to get stations", Toast.LENGTH_SHORT).show()
+                Log.d(TAG, "getStations error : ${it.message}")
+            }
+    }
+
+    fun getRoutes(context: Context,railwayType: String,stationCode : String){
+        val firestore = FirebaseFirestore.getInstance()
+        firestore.collection(railwayType)
+            .document(stationCode)
+            .collection(ProjectConstants.ROUTES)
+            .get()
+            .addOnSuccessListener {
+                Log.d(TAG, "routes are : ${it.documents}")
+                for(doc in it.documents){
+                    Log.d(TAG,"${doc.get(ProjectConstants.FAST)}")
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(context, "Failed to get routes", Toast.LENGTH_SHORT).show()
                 Log.d(TAG, "getStations error : ${it.message}")
             }
     }
