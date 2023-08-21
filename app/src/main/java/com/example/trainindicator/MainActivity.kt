8b33ea5b713455631db0d399598055acd0c93a82
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-08-21 19:57:48 +0530

    set theme to no dark mode

--- app/src/main/java/com/example/trainindicator/MainActivity.kt
+++ app/src/main/java/com/example/trainindicator/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.trainindicator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.trainindicator.databinding.ActivityMainBinding
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
